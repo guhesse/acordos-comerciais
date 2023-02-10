4a0c6ea47ebde284f25dbde43ef5380241da5fd6
guhesse <hesse.gustavo@gmail.com>
2023-02-10 00:14:18 -0300

    add search bar & search filter & fix categories filter

--- custom.js
+++ custom.js
@@ -1,15 +1,14 @@
+//Seleção por filterCategories
+
 var checkboxes = document.querySelectorAll(".categories input");
 for(var i = 0; i < checkboxes.length; i++) {
-  checkboxes[i].addEventListener("change", filter);
+  checkboxes[i].addEventListener("change", filterCategories);
 }
-var radios = document.getElementsByName("operation");
-for(var i = 0; i < radios.length; i++) {
-  radios[i].addEventListener("change", filter);
-}
-filter();
-function filter() {
+
+function filterCategories() {
   var i, j;
 
+  
   // Choose an operation
   const operation = document.getElementById("op-union").checked ? "union" : "intersection";
 
@@ -32,7 +31,6 @@ function filter() {
     if(categories.length == 0) {
       show = true;
     } else if(operation == "union") {
-
       show = true;
       for(j = 0; j < categories.length; j++) {
         if(!item.classList.contains(categories[j])) {
@@ -50,7 +48,67 @@ function filter() {
   }
 }
 
-function changeStyle(){
-  var element = document.getElementById(".options input");
-  element.style.display = "none";
+
+//Seleção por filterName
+document.getElementById("myInput").addEventListener("input", filterName);
+
+function filterName() {
+  var i, j;
+
+
+  //Remove Accents
+  function removeAccents(str) {
+    var mapaAcentosHex = {
+      a : /[\xE0-\xE6]/g,
+      A : /[\xC0-\xC6]/g,
+      e : /[\xE8-\xEB]/g,
+      E : /[\xC8-\xCB]/g,
+      i : /[\xEC-\xEF]/g,
+      I : /[\xCC-\xCF]/g,
+      o : /[\xF2-\xF6]/g,
+      O : /[\xD2-\xD6]/g,
+      u : /[\xF9-\xFC]/g,
+      U : /[\xD9-\xDC]/g,
+      c : /\xE7/g,
+      C : /\xC7/g,
+      n : /\xF1/g,
+      N : /\xD1/g
+    };
+   
+    for ( var letra in mapaAcentosHex ) {
+      var expressaoRegular = mapaAcentosHex[letra];
+      str = str.replace( expressaoRegular, letra );
+    }
+   
+    return str;
+  }
+  
+
+
+  // Get the entered name
+  var nameInput = document.getElementById("myInput").value.toUpperCase();
+
+  // Apply the filter
+  var items = document.querySelectorAll(".filterDiv");
+  var item, show;
+  for(i = 0; i < items.length; i++) {
+    item = items[i];
+    if(nameInput === "") {
+      show = true;
+    } else {
+      show = false;
+      if(removeAccents(item.getElementsByClassName("card-title")[0].innerText.toUpperCase()).includes(removeAccents(nameInput.toUpperCase()))) {
+        show = true;
+      }
+    }
+
+    if(show) {
+      item.classList.add("show");
+    } else {
+      item.classList.remove("show");
+    }
+  }
 }
+
+
+
